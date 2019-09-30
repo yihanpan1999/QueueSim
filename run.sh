ff0353e971e638bfe93ecadef1bdf5291fe7e4e4
JinJinGuang <43332709+JinJinGuang@users.noreply.github.com>
2019-09-30 08:24:08 +0800

    merge run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
-python Simulation.py --dataroot 'random' --policy 1 --ri 60
-python Simulation.py --dataroot 'random' --policy 4 --ri 60
-python Simulation.py --dataroot 'random' --policy 1 --ri 120
-python Simulation.py --dataroot 'random' --policy 4 --ri 120
-python Simulation.py --dataroot 'first_half' --policy 1 --ri 60
-python Simulation.py --dataroot 'first_half' --policy 4 --ri 60
-python Simulation.py --dataroot 'first_half' --policy 1 --ri 120
-python Simulation.py --dataroot 'first_half' --policy 4 --ri 120
-
+for i in {1..1}; do
+    echo python Simulation.py --dataroot "random" --policy 1 --ri 60
+    python Simulation.py --dataroot "random" --policy 1 --ri 60
+    echo python Simulation.py --dataroot "first_half" --policy 1 --ri 60
+    python Simulation.py --dataroot "first_half" --policy 1 --ri 60
+    echo python Simulation.py --dataroot "first_slots" --policy 1 --ri 60
+    python Simulation.py --dataroot "first_slots" --policy 1 --ri 60
+    echo python Simulation.py --dataroot "adaptive" --policy 1 --ri 60
+    python Simulation.py --dataroot "adaptive" --policy 1 --ri 60
+done 
